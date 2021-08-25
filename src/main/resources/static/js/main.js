08f5624de99771cbf3279d06e99497d1d209d54a
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-26 01:31:54 +0900

    created pop up page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,7 @@
 // TOP MENU SEARCH METHOD
 var searchEl = document.querySelector('.search');
 var searchInputEl = searchEl.querySelector('input');
+var middleBoxEl = document.querySelector('#middle-box');
 
 searchEl.addEventListener('click', function() {
     searchInputEl.focus();
@@ -8,10 +9,12 @@ searchEl.addEventListener('click', function() {
 
 searchInputEl.addEventListener('focus', function() {
     searchEl.classList.add('focused');
+    middleBoxEl.classList.add('search-focused');
 })
 
 searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused');
+    middleBoxEl.classList.remove('search-focused');
 })
 
 var menuOpenBtn = document.querySelector('#menu-open-btn');
@@ -28,3 +31,19 @@ menuCloseBtn.addEventListener('click', function() {
     sideBarEl.classList.remove('focused');
     topMenuEl.classList.remove('focused');
 })
+
+// POP UP LOGIN PAGE
+var loginBtn = document.querySelector('#login');
+var popupPageEl = document.querySelector('#popup');
+var closeLoginPageBtn = document.querySelector('.close-login-page-btn');
+
+loginBtn.addEventListener('click', function() {
+    popupPageEl.classList.add('popped');
+    closeLoginPageBtn.classList.add('popped');
+})
+
+closeLoginPageBtn.addEventListener('click', function() {
+    popupPageEl.classList.remove('popped');
+    closeLoginPageBtn.classList.remove('popped');
+})
+
