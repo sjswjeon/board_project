6a76de2c06a1bb1459d1a51bd4ec0ad8ee6ee40c
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-26 20:22:59 +0900

    board list

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,17 +23,20 @@ var sideBarEl = document.querySelector('.menu-bar');
 var menuCloseBtn = document.querySelector('#menu-close-btn');
 var topMenuEl = document.querySelector('#top-menu');
 var visualEl = document.querySelector('#visual');
+var secondBoxEl = document.querySelector('#second-box');
 
 menuOpenBtn.addEventListener('click', function() {
         sideBarEl.classList.add('focused');
         topMenuEl.classList.add('focused');
         visualEl.classList.add('side-menu-open');
+        secondBoxEl.classList.add('side-menu-open');
 })
 
 menuCloseBtn.addEventListener('click', function() {
     sideBarEl.classList.remove('focused');
     topMenuEl.classList.remove('focused');
     visualEl.classList.remove('side-menu-open');
+    secondBoxEl.classList.remove('side-menu-open');
 })
 
 // POP UP LOGIN PAGE
@@ -58,5 +61,40 @@ var visualHiddenBtn = document.querySelector('#visual-hidden-btn');
 
 visualHiddenBtn.addEventListener('click', function() {
     visualEl.classList.add('close-visual');
+    secondBoxEl.classList.add('close-visual');
 })
+
+// recent posts slide hidden
+
+var recentPostsEl = document.querySelector('#recent-posts');
+
+window.addEventListener('scroll', _.throttle(function() {
+
+    if (window.scrollY < 250 && !secondBoxEl.classList.contains('close-visual')) {
+        gsap.to(recentPostsEl, .6, {
+            opacity: 0,
+            display: 'none',
+            left: '-150px'
+        })
+    } else {
+        gsap.to(recentPostsEl, .6, {
+            opacity: 1,
+            display: 'block',
+            left: '0'
+        })
+    }
+}, 300));
+
+
+// BOARD TITLE CLICK OPEN CONTENT
+
+var boardTitleEl = document.querySelector('#board-title');
+var boardContentEl = document.querySelector('#board-content')
+
+boardTitleEl.addEventListener('click', function() {
+    gsap.to(boardContentEl, 0, {
+        display: 'flex'
+    })
+})
+
 
